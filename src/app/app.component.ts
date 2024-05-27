00b8157188d2d11bc71cc0eac51e82617c73f793
Michal Galas <michalgalas1989@gmail.com>
2024-05-27 15:38:05 +0200

    builds a structure and generates components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Portfolio';
+  title = 'Michal Galas';
 }
