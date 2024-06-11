c88f2718baf72fb37b02b48283db563ad3d0a067
Michal Galas <michalgalas1989@gmail.com>
2024-06-11 16:29:33 +0200

    added checkbox

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatCheckboxModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
