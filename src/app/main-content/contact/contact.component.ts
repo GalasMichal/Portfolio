0d627a68a12fc782913f407d3bd2be9bd752d5a8
Michal Galas <michalgalas1989@gmail.com>
2024-06-12 10:38:04 +0200

    added checkbox validierung

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CheckboxRequiredValidator, FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, MatCheckboxModule],
+  imports: [FormsModule, ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
