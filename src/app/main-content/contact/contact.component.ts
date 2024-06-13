959d2e620f153301b7a99f115ae15684b69d66c9
Michal Galas <michalgalas1989@gmail.com>
2024-06-13 15:49:13 +0200

    added footer and privacy policy with routing

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { CheckboxControlValueAccessor, CheckboxRequiredValidator, FormsModule, NgForm, NgModel } from '@angular/forms';
+import { FormsModule, NgForm, } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule, RouterOutlet],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
