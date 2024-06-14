c89d5289d7f3ae495be74c2cfd59588d566efd9d
Michal Galas <michalgalas1989@gmail.com>
2024-06-14 13:21:33 +0200

    added menu links with animations and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,34 @@
-import { Routes } from '@angular/router';
+import { Routes, ExtraOptions } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { MySkillsComponent } from './main-content/my-skills/my-skills.component';
+import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
+import { AboveTheFoldComponent } from './main-content/above-the-fold/above-the-fold.component';
+import { provideRouter, withRouterConfig } from '@angular/router';
+import { MenuComponent } from './shared/components/header/menu/menu/menu.component';
+
+// Definiere zusätzliche Optionen für den Router
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64] // Optional: um den Header auszugleichen
+};
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent},
-    { path: 'privacy-policy', component: PrivacyPolicyComponent}
+  {
+    path: '',
+    component: MainContentComponent,
+    children: [
+      { path: '', redirectTo: 'above-the-fold', pathMatch: 'full' },
+      { path: 'above-the-fold', component: AboveTheFoldComponent },
+      { path: 'about-me', component: AboutMeComponent },
+      { path: 'my-skills', component: MySkillsComponent },
+      { path: 'portfolio', component: PortfolioComponent }
+    ]
+  },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'menu', component: MenuComponent},
 ];
+
+// Router-Konfiguration bereitstellen
+export const routing = provideRouter(routes, withRouterConfig(routerOptions));
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './menu/menu/menu.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, RouterModule, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
